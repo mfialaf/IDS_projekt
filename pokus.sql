d6a756dcd12c10b9428bb8e598e5213c05c4b4d7
Marek Fiala <mafialka@gmail.com>
2020-03-25 17:16:14 +0100

    1.pokus

--- pokus.sql
+++ pokus.sql
@@ -1,1 +1,6 @@
-pokus
+CREATE TABLE "Hráč"
+          (
+              "id" INT GENERATED AS IDENTITY NOT NULL PRIMARY KEY,
+              "jméno" VARCHAR(160) NOT NULL,
+              "věk" INT NOT NULL
+          )
