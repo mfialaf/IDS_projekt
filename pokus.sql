441657181a0cc8d4db20671a590d5ebd2274cd99
Marek Fiala <mafialka@gmail.com>
2020-03-26 13:44:53 +0100

    Diakritika, stredniky

--- pokus.sql
+++ pokus.sql
@@ -1,67 +1,67 @@
-CREATE TABLE kočka
+CREATE TABLE kocka
           (
-              hlavní_jméno VARCHAR(160) NOT NULL PRIMARY KEY,
-              vzorek_kůže INT NOT NULL,
+              hlavni_jmeno VARCHAR(160) NOT NULL PRIMARY KEY,
+              vzorek_kuze INT NOT NULL,
               barva_srsti VARCHAR(50) NOT NULL
-          )
+          );
 
-CREATE TABLE život
+CREATE TABLE zivot
           (
-              ID_život INT NOT NULL PRIMARY KEY, -- regex na max 9
-              pořadí INT NOT NULL,
-              délka INTERVAL NOT NULL,
-              místo_narození VARCHAR(50) NOT NULL
-          )
+              ID_zivot INT NOT NULL PRIMARY KEY, -- regex na max 9
+              poradi INT NOT NULL,
+              delka INT NOT NULL,
+              misto_narozeni VARCHAR(50) NOT NULL
+          );
 
 CREATE TABLE teritorium
           (
               ID_teritorium VARCHAR(50) NOT NULL PRIMARY KEY,
               typ_teritoria VARCHAR(50) NOT NULL,
-              kapacita_koček INT NOT NULL,
-          )
+              kapacita_kocek INT NOT NULL
+          );
 
-CREATE TABLE vlastnictví
+CREATE TABLE vlastnictvi
           (
-              ID_valstnictví VARCHAR(50) NOT NULL PRIMARY KEY,
-              typ_vlastnictví VARCHAR(50) NOT NULL,
-              kvantita INT NOT NULL,
-          )
+              ID_valstnictvi VARCHAR(50) NOT NULL PRIMARY KEY,
+              typ_vlastnictvi VARCHAR(50) NOT NULL,
+              kvantita INT NOT NULL
+          );
 
 CREATE TABLE hostitel
           (
               ID_hostitel VARCHAR(50) NOT NULL PRIMARY KEY,
-              jméno VARCHAR(50) NOT NULL,
-              věk INT NOT NULL,
-              pohlaví INT(1),   -- Oracle nema bolean nakze napr Muž - 1, Žena - 0
-              místo_bydlení VARCHAR(50) NOT NULL,
-          )
+              jmeno VARCHAR(50) NOT NULL,
+              vek INT NOT NULL,
+              pohlavi INT(1),   -- Oracle nema bolean nakze napr Muž - 1, Žena - 0
+              misto_bydleni VARCHAR(50) NOT NULL
+          );
 
 CREATE TABLE rasa
           (
               ID_typ VARCHAR(50) NOT NULL PRIMARY KEY,
-              půvpd VARCHAR(50) NOT NULL,
-              max_délka_tesáků INT NOT NULL,
-          )
+              puvpd VARCHAR(50) NOT NULL,
+              max_delka_tesaku INT NOT NULL
+          );
 
-CREATE TABLE specifické_rysy
+CREATE TABLE specificke_rysy
           (
               ID_rysy VARCHAR(50) NOT NULL PRIMARY KEY,
-              barva_očí VARCHAR(50) NOT NULL,
-          )
+              barva_oci VARCHAR(50) NOT NULL
+          );
 
 --- TABULKY VAZEB ---------
 
 CREATE TABLE interval_pobytu
           (
               doba TIME NOT NULL PRIMARY KEY
-          )
+          );
 
-CREATE TABLE interval_vlastnictví
+CREATE TABLE interval_vlastnictvi
           (
               doba TIME NOT NULL PRIMARY KEY
-          )
+          );
 
-CREATE TABLE nazývá
+CREATE TABLE nazyva
           (
-              přezdívka VARCHAR(50) NOT NULL PRIMARY KEY
+              prezdivka VARCHAR(50) NOT NULL PRIMARY KEY
           )
