eb7ee0a2fd01e76ea51e7b23aefc6478c09c7998
Marek Fiala <mafialka@gmail.com>
2020-03-27 14:34:56 +0100

    nevim co k tomu napsat, sem baller

--- pokus.sql
+++ pokus.sql
@@ -26,11 +26,11 @@ CREATE TABLE Kocka
 
 CREATE TABLE Zivot
         (
-            ID_zivot VARCHAR(10) NOT NULL PRIMARY KEY, -- regex na max 9
+            ID_zivot VARCHAR(10) NOT NULL PRIMARY KEY,
             poradi INT NOT NULL,
             delka VARCHAR(10) NOT NULL,
 
-            ID_kocky VARCHAR(160) NOT NULL -- FK kocky
+            jmeno_kocky VARCHAR(160) NOT NULL -- FK kocky
         );
 
 CREATE TABLE Teritorium
@@ -42,12 +42,12 @@ CREATE TABLE Teritorium
 
 CREATE TABLE Vlastnictvi
         (
-            ID_valstnictvi VARCHAR(4) NOT NULL PRIMARY KEY,
+            ID_vlastnictvi VARCHAR(4) NOT NULL PRIMARY KEY,
             typ_vlastnictvi VARCHAR(50) NOT NULL,
             kvantita INT NOT NULL,
 
             ID_hostitele VARCHAR(4), -- FK hostitele
-            ID_kocky VARCHAR(160), -- FK kocky
+            jmeno_kocky VARCHAR(160), -- FK kocky
             ID_teritoria VARCHAR(4) NOT NULL -- FK teritoria
         );
 
@@ -56,7 +56,7 @@ CREATE TABLE Hostitel
             ID_hostitel VARCHAR(4) NOT NULL PRIMARY KEY,
             jmeno VARCHAR(50) NOT NULL,
             vek INT NOT NULL,
-            pohlavi INT NOT NULL,   -- Oracle nema bolean nakze napr Muž - 1, Žena - 0
+            pohlavi INT NOT NULL,
             misto_bydleni VARCHAR(50) NOT NULL
         );
 
@@ -78,9 +78,9 @@ CREATE TABLE Specificke_rysy
 
 CREATE TABLE Pohyb_kocky
         (
-            interval_pobytu VARCHAR(10) NOT NULL PRIMARY KEY, -- TIME????????
+            interval_pobytu VARCHAR(10) NOT NULL PRIMARY KEY,
 
-            ID_kocky VARCHAR(160) NOT NULL, --FK kocky
+            jmeno_kocky VARCHAR(160) NOT NULL, --FK kocky
             ID_teritoria VARCHAR(4) NOT NULL  --Fk teritoria
         );
 
@@ -88,7 +88,7 @@ CREATE TABLE Interval_vlastnictvi
         (
             doba VARCHAR(10) NOT NULL PRIMARY KEY,
 
-            ID_kocky VARCHAR(160) NOT NULL, --FK kocky
+            jmeno_kocky VARCHAR(160) NOT NULL, --FK kocky
             ID_vlastnictvi VARCHAR(4) NOT NULL  --FK vlastnictvi
         );
 
@@ -96,14 +96,14 @@ CREATE TABLE Slouzi
         (
             prezdivka VARCHAR(50) NOT NULL PRIMARY KEY,
 
-            ID_kocky VARCHAR(160) NOT NULL, --FK kocky
+            jmeno_kocky VARCHAR(160) NOT NULL, --FK kocky
             ID_hostitele VARCHAR(4) NOT NULL --FK hostitel
         );
 
 CREATE TABLE Rysy_rasy
         (
             ID_rasy VARCHAR(4) NOT NULL, -- FK rasy
-            ID_rysy VARCHAR(50) NOT NULL  -- FK     --------------- tady nevim jestli dat 4 nebo ne
+            ID_rysy VARCHAR(50) NOT NULL  -- FK
         );
 
 CREATE TABLE Preference
@@ -132,20 +132,20 @@ CREATE TABLE Aktualni
 -- kocky --
     ALTER TABLE Kocka ADD CONSTRAINT fk_je_rasy FOREIGN KEY (ID_rasy) REFERENCES Rasa;
 -- zivot --
-    ALTER TABLE Zivot ADD CONSTRAINT fk_ma_kocku FOREIGN KEY (ID_kocky) REFERENCES Kocka;
+    ALTER TABLE Zivot ADD CONSTRAINT fk_ma_kocku FOREIGN KEY (jmeno_kocky) REFERENCES Kocka;
 -- vlastnictvi --
     ALTER TABLE Vlastnictvi ADD CONSTRAINT fk_je_propujceno FOREIGN KEY (ID_hostitele) REFERENCES Hostitel;
-    ALTER TABLE Vlastnictvi ADD CONSTRAINT fk_ma FOREIGN KEY (ID_kocky) REFERENCES Kocka;
+    ALTER TABLE Vlastnictvi ADD CONSTRAINT fk_ma FOREIGN KEY (jmeno_kocky) REFERENCES Kocka;
     ALTER TABLE Vlastnictvi ADD CONSTRAINT fk_se_nachazi FOREIGN KEY (ID_teritoria) REFERENCES Teritorium;
 -- M:N vazby --
 --pohyb_kocky--
-    ALTER TABLE Pohyb_kocky ADD CONSTRAINT fk_se_pohybuje FOREIGN KEY (ID_kocky) REFERENCES Kocka;
+    ALTER TABLE Pohyb_kocky ADD CONSTRAINT fk_se_pohybuje FOREIGN KEY (jmeno_kocky) REFERENCES Kocka;
     ALTER TABLE Pohyb_kocky ADD CONSTRAINT fk_v_prostredi FOREIGN KEY (ID_teritoria) REFERENCES Teritorium;
 --interval_vlastnictvi--
-    ALTER TABLE Interval_vlastnictvi ADD CONSTRAINT fk_je_vlastneno FOREIGN KEY (ID_kocky) REFERENCES Kocka;
+    ALTER TABLE Interval_vlastnictvi ADD CONSTRAINT fk_je_vlastneno FOREIGN KEY (jmeno_kocky) REFERENCES Kocka;
     ALTER TABLE Interval_vlastnictvi ADD CONSTRAINT fk_pripada FOREIGN KEY (ID_vlastnictvi) REFERENCES Vlastnictvi;
 --slouzi--
-    ALTER TABLE Slouzi ADD CONSTRAINT fk_slouzi FOREIGN KEY (ID_kocky) REFERENCES Kocka;
+    ALTER TABLE Slouzi ADD CONSTRAINT fk_slouzi FOREIGN KEY (jmeno_kocky) REFERENCES Kocka;
     ALTER TABLE Slouzi ADD CONSTRAINT fk_je_panem FOREIGN KEY (ID_hostitele) REFERENCES Hostitel;
 --rysy_rasy--
     ALTER TABLE Rysy_rasy ADD CONSTRAINT fk_rasa_ma FOREIGN KEY (ID_rasy) REFERENCES Rasa;
@@ -175,12 +175,12 @@ CREATE TABLE Aktualni
 
 
 
-INSERT INTO Rasa (ID_rasy, puvod, max_delka_tesaku) VALUES ('R478', 'Egypt', 27); -- regex na cm?
+INSERT INTO Rasa (ID_rasy, puvod, max_delka_tesaku) VALUES ('R478', 'Egypt', 27);
 INSERT INTO Kocka (hlavni_jmeno, vzorek_kuze, barva_srsti, ID_rasy) VALUES ('julca','BLK', 'fialova', 'R478');
-INSERT INTO Zivot (ID_zivot, poradi, delka, ID_kocky) VALUES ('Z123', 1, '13r254d', 'julca'); -- regex na rok a dny
+INSERT INTO Zivot (ID_zivot, poradi, delka, jmeno_kocky) VALUES ('Z123', 1, '13r254d', 'julca');
 INSERT INTO Teritorium (ID_teritorium, typ_teritoria, kapacita_kocek) VALUES ('T991', 'obyvacka', 20);
 INSERT INTO Hostitel (ID_hostitel, jmeno, vek, pohlavi, misto_bydleni) VALUES ('H005', 'Pavel', 25, 1, 'Znojmo');
-INSERT INTO Vlastnictvi (ID_valstnictvi, typ_vlastnictvi, kvantita, ID_hostitele, ID_kocky, ID_teritoria) VALUES ('V845', 'balonek', 3, 'H005', 'julca', 'T991');
+INSERT INTO Vlastnictvi (ID_vlastnictvi, typ_vlastnictvi, kvantita, ID_hostitele, jmeno_kocky, ID_teritoria) VALUES ('V845', 'balonek', 3, 'H005', 'julca', 'T991');
 INSERT INTO Specificke_rysy (ID_rysy, barva_oci) VALUES ('S247', 'zelena');
 
 
@@ -188,10 +188,12 @@ INSERT INTO Specificke_rysy (ID_rysy, barva_oci) VALUES ('S247', 'zelena');
 ---- CHECKy
  ALTER TABLE Hostitel ADD CONSTRAINT check_pohlavi CHECK ((pohlavi = 0) OR (pohlavi = 1));
  ALTER TABLE Hostitel ADD CONSTRAINT check_vek CHECK ((vek >= 1) AND (vek <= 130));
+ ALTER TABLE Zivot ADD CONSTRAINT check_pocet_zivotu CHECK ((poradi >= 1) AND (poradi <= 9));
+ ALTER TABLE Zivot ADD CONSTRAINT check_zapis_zivota CHECK (REGEXP_LIKE(delka, '^([0-9]{1,2}[r,R]){0,1}(([0-9]{1,2}|[1-2][0-9]{2}|[3][0-6][0-5])[d,D]){0,1}$'));
 
  ALTER TABLE Zivot ADD CONSTRAINT check_ID_1 CHECK (REGEXP_LIKE(ID_zivot,'Z[0-9]{3}'));
  ALTER TABLE Teritorium ADD CONSTRAINT check_ID_2 CHECK (REGEXP_LIKE(ID_teritorium,'T[0-9]{3}'));
- ALTER TABLE Vlastnictvi ADD CONSTRAINT check_ID_3 CHECK (REGEXP_LIKE(ID_valstnictvi,'V[0-9]{3}'));
+ ALTER TABLE Vlastnictvi ADD CONSTRAINT check_ID_3 CHECK (REGEXP_LIKE(ID_vlastnictvi,'V[0-9]{3}'));
  ALTER TABLE Hostitel ADD CONSTRAINT check_ID_4 CHECK (REGEXP_LIKE(ID_hostitel,'H[0-9]{3}'));
  ALTER TABLE Rasa ADD CONSTRAINT check_ID_5 CHECK (REGEXP_LIKE(ID_rasy,'R[0-9]{3}'));
  ALTER TABLE Specificke_rysy ADD CONSTRAINT check_ID_6 CHECK (REGEXP_LIKE(ID_rysy,'S[0-9]{3}'));
